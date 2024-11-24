70db9d635da9e9c9040de76cf5730c67fe243573
VedG08 <vedantgandhi2001@gmail.com>
2024-11-24 15:19:14 +0530

    MainTab structure

---
+++ app/(tabs)/map/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MapIndex = () => {
+  return (
+    <View>
+      <Text>MapIndex</Text>
+    </View>
+  );
+};
+
+export default MapIndex;
---
+++ app/(tabs)/user-creation/_layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+const ExtraLayout = () => {
+  return <Stack />;
+};
+
+export default ExtraLayout;
