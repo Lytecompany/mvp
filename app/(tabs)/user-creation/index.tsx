848372abc705e4c0a4615001eadcfdbda450a99b
Rohan Bhatia <lytecompany04@gmail.com>
2024-11-27 10:09:29 +0400

    Made the Profile pictures Card

--- app/(tabs)/user-creation/index.tsx
+++ app/(tabs)/user-creation/index.tsx
@@ -1,4 +1,5 @@
-import {  View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import {  View, Text, StyleSheet, Image, ScrollView, Button, Modal, StatusBar} from "react-native";
+import { Stack } from "expo-router";
 import React, { useState } from "react";
 import HeaderTitleUserCreation from "@/components/HeaderTitleUserCreation";
 import OtpField from "@/components/OtpInputUserCreation";
@@ -6,62 +7,59 @@ import InputField from "@/components/InputFieldUserCreation";
 import { GENDER_OPTIONS, STAR_SIGN } from "@/constants/Data";
 import UnitToggle from "@/components/ToggleButtonUserCreation";
 import ProfileCircle from "@/components/LikesYouComponentChatPage";
-import Card from "@/components/ChatCard";
+// import Card from "@/components/ChatCard";
 import ChatItem from "@/components/ChatItem";
+import ProfileCard from "@/components/ProfilePicturesCard";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 
 
 
 const UserCreate = () => {
-  const chats = [
-    { name: "Rohan Bhatia", message: "Hey Sexy" },
-    { name: "Vedant Gandhi", message: "Hey baby doll, Wanna come over?" },
-    { name: "Vaibhav K", message: "Hey I wanna open up tonight" },
-    { name: "Nihal S", message: "Can't talk today" },
-    { name: "Fenil", message: "What is Strategy?" },
-    { name: "Asit", message: "Have a doubt, can you join?" },
-  ];
+const photo1 = require('@/assets/images/2.png');
+const photo2 = require('@/assets/images/StarSignImages/gemini.png');
+const photo3 = require('@/assets/images/StarSignImages/aries.png');
+const photo4 = require('@/assets/images/StarSignImages/cancer.png');
+  const [isModalVisible, setModalVisible] = useState(false);
   
   return (
-    <View style = {styles.container}>
-        <View style = {styles1.container}>
-        <ProfileCircle isHighlighted = {true}/>
-        <ProfileCircle/>
-        <ProfileCircle/>
+    // <View style = {styles.container}>
+    //     <View style = {styles1.container}>
+    //     <ProfileCircle isHighlighted = {true}/>
+    //     <ProfileCircle/>
+    //     <ProfileCircle/>
+    //     </View>
+    //     <Card title="Chats">
+    //       {chats.map((chat, index) => (
+    //         <ChatItem key={index} name={chat.name} message={chat.message} />
+    //       ))}
+    //     </Card> 
+    // </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+    <ScrollView>
+      <View style = {styles.container}>
+      <Stack.Screen options={{ headerShown: false }} />
+      <ProfileCard name="Rohan" age={23} pronouns="He/Him" 
+      photoSource={[photo1, photo2, photo3, photo4]}
+      />
         </View>
-        <Card title="Chats">
-          {chats.map((chat, index) => (
-            <ChatItem key={index} name={chat.name} message={chat.message} />
-          ))}
-        </Card> 
-    </View>
+    </ScrollView>
+    </GestureHandlerRootView>
   );
+
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 11,
-    paddingVertical: 10,
-    backgroundColor: "rgba(254, 214, 187, 0.80)", // Fallback color
-    borderTopColor: "rgba(230, 230, 230, 0.80)",
-    borderBottomColor: "rgba(230, 230, 230, 0.80)",
-    shadowColor: "#FDE3C8",
-    shadowOpacity: 0.5,
-    shadowOffset: { height: 1, width: 0.9 }, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 60,
+    color: "FEDEC9",
+    paddingHorizontal: 9
+
   },
 });
-
-const styles1 = StyleSheet.create({
-  container: {
-    flexDirection: "row",},
-  profileCircle: {
-    marginHorizontal: 10
-  },
-
-  }
-);
-
 
 
 export default UserCreate
